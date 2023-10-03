50b92161ce928da812e0fd3381ab6a8d9bd0abc0
lukazmm <lukas.m.mesicek@gmail.com>
2023-10-03 13:02:19 -0600

    Some progress on push_back

--- kernel/verified/linked_list.rs
+++ kernel/verified/linked_list.rs
@@ -47,8 +47,13 @@ impl<T: Default> LinkedList<T> {
 
     //start of tmp
     pub closed spec fn view(&self) -> Seq<T>
+        recommends self.wf()
     {
-        Seq::new(self.ptrs@.len(), |i: int| self.perms@[self.ptrs@[i].page_pptr()]@.value_at_opt(self.ptrs@[i].index()).unwrap().value)
+        Seq::new(self.ptrs@.len(), |i: int| {
+            let ptr = self.ptrs@[i];
+            let arena: &Arena<T> = &self.perms@[ptr.page_pptr()]@;
+            arena.value_at(ptr.index()).value
+        })
     }
 
     pub closed spec fn unique(&self) -> bool
@@ -56,9 +61,9 @@ impl<T: Default> LinkedList<T> {
         forall|i:int, j:int| i != j && 0<=i<self@.len() && 0<=j<self@.len() ==> self@[i] != self@[j]
     }
 
-    pub closed spec fn len(&self) -> int
+    pub closed spec fn len(&self) -> nat
     {
-        223
+        self.ptrs@.len()
     }
 
     pub closed spec fn page_closure(&self) -> Set<PagePtr>
@@ -75,8 +80,17 @@ impl<T: Default> LinkedList<T> {
     {
         arbitrary()
     }
+
+    pub closed spec fn capacity(&self) -> nat {
+        self.free_ptrs@.len()
+    }
+
     //end of tmp
     //@Lukas: just change to whatever you want
+
+    // *********************
+    // API *****************
+    // *********************
 
     pub fn new() -> (ret: Self)
         ensures ret.wf()
@@ -95,6 +109,56 @@ impl<T: Default> LinkedList<T> {
 
             perms: Ghost(Map::empty()),
         }
+    }
+
+    pub fn push_back(&mut self, v: T) 
+        requires
+            old(self).wf(),
+            old(self).capacity() > 0
+        ensures
+            self.wf(),
+            // self@ == old(self)@.push(v)
+    {
+
+    }
+
+    fn push_back_empty(&mut self, v: T) 
+    requires
+        old(self).wf(),
+        old(self).capacity() > 0,
+        old(self).len() == 0,
+    ensures
+        self.wf(),
+        // self@ == old(self)@.push(v)
+    {
+        // From capacity we 
+        
+    }
+
+    fn pop_free_ptr(&mut self)
+        requires
+            old(self).wf(),
+            old(self).capacity() == 1,
+            old(self).len() == 0,
+        ensures
+            self.wf(),
+            self@ == old(self)@,
+            self.capacity() == 0,
+    {
+        assert(self.free_head.is_some() && self.free_tail.is_some());
+
+        // Update ghost state
+        
+        proof {
+            self.free_ptrs@ = Seq::empty();
+        }
+
+        // let result = self.free_tail.clone().unwrap();
+
+        self.free_tail = None;
+        self.free_head = None;
+        
+        // return result;
     }
 
     // ********************
@@ -117,51 +181,52 @@ impl<T: Default> LinkedList<T> {
         }
     }
 
-    // spec fn free_prev_of(&self, i: nat) -> Option<NodePtr<T>> {
-    //     if i == 0 {
-    //         None::<NodePtr<T>>
-    //     } else {
-    //         Some(self.free_ptrs@[i - 1])
-    //     }
-    // }
+    spec fn free_prev_of(&self, i: nat) -> Option<NodePtr<T>> {
+        if i == 0 {
+            None::<NodePtr<T>>
+        } else {
+            Some(self.free_ptrs@[i - 1int])
+        }
+    }
 
-    // spec fn free_next_of(&self, i: nat) -> Option<NodePtr<T>> {
-    //     if i + 1 == self.free_ptrs@.len() {
-    //         None::<NodePtr<T>>
-    //     } else {
-    //         Some(self.free_ptrs@[i + 1int])
-    //     }
-    // }
+    spec fn free_next_of(&self, i: nat) -> Option<NodePtr<T>> {
+        if i + 1 == self.free_ptrs@.len() {
+            None::<NodePtr<T>>
+        } else {
+            Some(self.free_ptrs@[i + 1int])
+        }
+    }
 
-    // spec fn page_next_of(&self, i: nat) -> Option<PageNodePtr> {
-    //     if i + 1 == self.page_ptrs@.len() {
-    //         None::<PageNodePtr>
-    //     } else {
-    //         Some(self.page_ptrs@[i + 1int])
-    //     }
-    // }
+    spec fn page_next_of(&self, i: nat) -> Option<PageNodePtr> {
+        if i + 1 == self.page_ptrs@.len() {
+            None::<PageNodePtr>
+        } else {
+            Some(self.page_ptrs@[i + 1int])
+        }
+    }
 
     // ********************
     // Well Formed Spec ***
     // ********************
 
     pub closed spec fn wf(&self) -> bool {
-        self.wf_perms() && self.wf_ptrs()
+        self.wf_perm_map() && self.wf_ptrs() && self.wf_free_ptrs() && self.wf_page_ptrs()
     }
 
-
-    spec fn wf_perms(&self) -> bool {
+    // Ensures the wf perm map is valid at all times
+    spec fn wf_perm_map(&self) -> bool {
         &&& forall|i: PagePPtr| self.perms@.dom().contains(i) ==> #[trigger] self.wf_perm(i)
     }
 
     spec fn wf_perm(&self, ptr: PagePPtr) -> bool {
         let arena: &Arena<T> = &self.perms@[ptr]@;
-
+        // Key matches arena pointed to
         &&& ptr.id() == arena.page_base()
     }
 
+    // Ensures each ptr is valid
     spec fn wf_ptrs(&self) -> bool {
-        self.wf_head() && self.wf_tail() && forall|i: nat| 0 <= i < self.ptrs@.len() ==> self.wf_ptr(i)
+        self.wf_head() && self.wf_tail() && forall|i: nat| 0 <= i < self.ptrs@.len() ==> #[trigger] self.wf_ptr(i)
     }
 
     spec fn wf_ptr(&self, i: nat) -> bool {
@@ -170,7 +235,6 @@ impl<T: Default> LinkedList<T> {
         let node = arena.value_at(ptr.index());
         node.prev == self.prev_of(i) && node.next == self.next_of(i)  
     }
-
 
     spec fn wf_head(&self) -> bool {
         if self.ptrs@.len() == 0 {
@@ -188,53 +252,51 @@ impl<T: Default> LinkedList<T> {
         }
     }
 
-    // spec fn wf_free_ptrs(&self) -> bool {
-    //     self.wf_free_head() && self.wf_free_tail() && forall|i: nat| 0 <= i < self.free_ptrs@.len() ==> self.wf_free_ptr(i) 
-    // }
+    // Ensures each free ptr is valid
+    spec fn wf_free_ptrs(&self) -> bool {
+        self.wf_free_head() && self.wf_free_tail() && forall|i: nat| 0 <= i < self.free_ptrs@.len() ==> #[trigger] self.wf_free_ptr(i)
+    }
 
-    // spec fn wf_free_ptr(&self, i: nat) -> bool {
-    //     let ptr: &NodePtr<T> = &self.free_ptrs@[i as int];
-    //     let arena: &Arena<T> = &self.perms@[ptr.page_pptr()]@;
-        
-    //     match arena.value_at_opt(ptr.index()) {
-    //         Some(node) => node.prev == self.free_prev_of(i) && node.next == self.free_next_of(i),
-    //         None => false,
-    //     }        
-    // }
+    spec fn wf_free_ptr(&self, i: nat) -> bool {
+        let ptr: &NodePtr<T> = &self.free_ptrs@[i as int];
+        let arena: &Arena<T> = &self.perms@[ptr.page_pptr()]@;
+        let node = arena.value_at(ptr.index());
+        node.prev == self.prev_of(i) && node.next == self.next_of(i)  
+    }
 
-    // spec fn wf_free_head(&self) -> bool {
-    //     if self.free_ptrs@.len() == 0 {
-    //         self.free_head == None::<NodePtr<T>>
-    //     } else {
-    //         self.free_head == Some(self.free_ptrs@[0])
-    //     }
-    // }
+    spec fn wf_free_head(&self) -> bool {
+        if self.ptrs@.len() == 0 {
+            self.head == None::<NodePtr<T>>
+        } else {
+            self.head == Some(self.free_ptrs@[0])
+        }
+    }
 
-    // spec fn wf_free_tail(&self) -> bool {
-    //     if self.free_ptrs@.len() == 0 {
-    //         self.free_tail == None::<NodePtr<T>>
-    //     } else {
-    //         self.free_tail == Some(self.free_ptrs@[self.free_ptrs@.len() - 1])
-    //     }
-    // }
+    spec fn wf_free_tail(&self) -> bool {
+        if self.free_ptrs@.len() == 0 {
+            self.tail == None::<NodePtr<T>>
+        } else {
+            self.tail == Some(self.free_ptrs@[self.free_ptrs@.len() - 1])
+        }
+    }
 
-    // spec fn wf_page_ptrs(&self) -> bool {
-    //     self.wf_page_head() && forall |i: nat| 0 <= i < self.page_ptrs@.len() ==> self.wf_page_ptr(i)
-    // }
+    spec fn wf_page_ptrs(&self) -> bool {
+        self.wf_page_head() && forall |i: nat| 0 <= i < self.page_ptrs@.len() ==> #[trigger] self.wf_page_ptr(i)
+    }
 
-    // spec fn wf_page_ptr(&self, i: nat) -> bool {
-    //     let ptr: &PageNodePtr = &self.page_ptrs@[i as int];
-    //     let arena: &Arena<T> = &self.perms@[ptr.page_pptr()]@;
-    //     arena.metadata().next == self.page_next_of(i)     
-    // }
+    spec fn wf_page_ptr(&self, i: nat) -> bool {
+        let ptr: &PageNodePtr = &self.page_ptrs@[i as int];
+        let arena: &Arena<T> = &self.perms@[ptr.page_pptr()]@;
+        arena.metadata().next == self.page_next_of(i)     
+    }
 
-    // spec fn wf_page_head(&self) -> bool {
-    //     if self.page_ptrs@.len() == 0 {
-    //         self.page_head == None::<PageNodePtr>
-    //     } else {
-    //         self.page_head == Some(self.page_ptrs@[0])
-    //     }
-    // }
+    spec fn wf_page_head(&self) -> bool {
+        if self.page_ptrs@.len() == 0 {
+            self.page_head == None::<PageNodePtr>
+        } else {
+            self.page_head == Some(self.page_ptrs@[0])
+        }
+    }
 }
 
 }
