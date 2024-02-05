1f63c14674ecb1ed87d90a3094eef7d5d2685019
FeizaiYiHao <xiangdc@bu.edu>
2024-02-04 22:36:14 -0700

    add proc kernel specs

--- kernel/verified/kernel/kernel_spec.rs
+++ kernel/verified/kernel/kernel_spec.rs
@@ -39,10 +39,25 @@ impl Kernel{
         )
     }
 
+    pub open spec fn kernel_proc_mmu_wf(&self) -> bool{
+        &&&
+        (forall|pcid:Pcid|#![auto] self.proc_man.get_pcid_closure().contains(pcid) ==> 
+            (0 <= pcid< PCID_MAX && self.mmu_man.get_free_pcids_as_set().contains(pcid) == false))
+        &&&
+        (self.proc_man.get_ioid_closure() =~= self.mmu_man.get_iommu_ids())
+    }
+
+    pub open spec fn kernel_proc_no_thread_in_transit(&self) -> bool{
+        &&&
+        (forall|thread_ptr:ThreadPtr|#![auto] self.proc_man.get_thread_ptrs().contains(thread_ptr) ==> 
+            self.proc_man.get_thread(thread_ptr).state != TRANSIT)
+    }
     pub open spec fn kernel_mem_layout_wf(&self) -> bool {
         // all pages used to construct pagetable/iommutables are marked correctly in page allocator 
         &&&
         (self.page_alloc.get_page_table_pages() =~= self.mmu_man.get_mmu_page_closure())
+        &&&
+        (self.page_alloc.get_allocated_pages() =~= self.proc_man.get_proc_man_page_closure())
     }
 
     pub proof fn pagetable_mem_wf_derive(&self)
@@ -64,7 +79,11 @@ impl Kernel{
         );
     }
 
-    pub open spec fn wf(&self) -> bool{
+    pub open spec fn wf(&self) -> bool{        
+        &&&
+        (
+            self.proc_man.wf()
+        )
         &&&
         (
             self.mmu_man.wf()
@@ -85,6 +104,10 @@ impl Kernel{
         (
             self.kernel_mmu_page_alloc_iommutable_wf()
         )
+        &&&
+        (self.kernel_proc_mmu_wf())
+        &&&
+        (self.kernel_proc_no_thread_in_transit())
     }
 
 }
