10f9fc2e1f97165c7717299aad933e8b713fbd9a
FeizaiYiHao <xiangdc@bu.edu>
2024-02-27 09:58:20 -0700

    fix new proc return values

--- kernel/verified/kernel/syscall_proc/syscall_new_proc.rs
+++ kernel/verified/kernel/syscall_proc/syscall_new_proc.rs
@@ -59,15 +59,16 @@ impl Kernel {
                 &&
                 // the new proccess's pagetable is empty. There are kernel and loader mapped in this new pagetable, but they are transparent to user level
                 forall|va:VAddr| #![auto] spec_va_valid(va) ==> self.mmu_man.get_pagetable_by_pcid(self.proc_man.get_proc(self.proc_man.get_thread(ret.2.unwrap()).parent).pcid).get_pagetable_mapping()[va].is_None()),
-    {   
+    { 
+        let (default_pcid, default_cr3) = self.mmu_man.get_reserved_pcid_and_cr3();  
         if cpu_id >= NUM_CPUS{
             assert(self == old(self));
-            return (SyscallReturnStruct::new(CPU_ID_INVALID,0,0,pt_regs),None,None);
+            return (SyscallReturnStruct::new(CPU_ID_INVALID,default_pcid,default_cr3,pt_regs),None,None);
         }
 
         if self.cpu_list.get(cpu_id).get_is_idle() {
             assert(self == old(self));
-            return (SyscallReturnStruct::new(NO_RUNNING_THREAD,0,0,pt_regs),None,None);
+            return (SyscallReturnStruct::new(NO_RUNNING_THREAD,default_pcid,default_cr3,pt_regs),None,None);
         }
         assert(self.cpu_list[cpu_id as int].get_is_idle() == false);
         let current_thread_ptr_op = self.cpu_list.get(cpu_id).get_current_thread();
--- kernel/verified/kernel/syscall_proc/syscall_new_proc_with_iommu.rs
+++ kernel/verified/kernel/syscall_proc/syscall_new_proc_with_iommu.rs
@@ -18,18 +18,19 @@ use crate::kernel::*;
 impl Kernel {
 
 
-    pub fn syscall_new_proc_with_iommu(&mut self, cpu_id:CPUID, pt_regs: PtRegs, endpoint_index: EndpointIdx, pt_regs_new_proc: PtRegs) -> (ret:(SyscallReturnStruct,Option<ProcPtr>,Option<ThreadPtr>))
+    pub fn syscall_new_proc_with_iommu(&mut self, cpu_id:CPUID, pt_regs: PtRegs, endpoint_index: EndpointIdx, pt_regs_new_proc: PtRegs) -> (ret:(SyscallReturnStruct,Option<usize>))
         requires
             old(self).wf(),
         ensures
             self.wf(),
     {   
+        let (default_pcid, default_cr3) = self.mmu_man.get_reserved_pcid_and_cr3();
         if cpu_id >= NUM_CPUS{
-            return (SyscallReturnStruct::new(CPU_ID_INVALID,0,0,pt_regs),None,None);
+            return (SyscallReturnStruct::new(CPU_ID_INVALID,default_pcid,default_cr3,pt_regs),None);
         }
 
         if self.cpu_list.get(cpu_id).get_is_idle() {
-            return (SyscallReturnStruct::new(NO_RUNNING_THREAD,0,0,pt_regs),None,None);
+            return (SyscallReturnStruct::new(NO_RUNNING_THREAD,default_pcid,default_cr3,pt_regs),None);
         }
         assert(self.cpu_list[cpu_id as int].get_is_idle() == false);
         let current_thread_ptr_op = self.cpu_list.get(cpu_id).get_current_thread();
@@ -40,41 +41,42 @@ impl Kernel {
         let cr3 = self.mmu_man.get_cr3_by_pcid(pcid);
         
         if endpoint_index >= MAX_NUM_ENDPOINT_DESCRIPTORS{
-            return (SyscallReturnStruct::new(ENDPOINT_INDEX_INVALID,pcid,cr3,pt_regs),None,None);
+            return (SyscallReturnStruct::new(ENDPOINT_INDEX_INVALID,pcid,cr3,pt_regs),None);
         }
 
         let target_endpoint_ptr = self.proc_man.get_thread_endpoint_ptr_by_endpoint_idx(current_thread_ptr, endpoint_index);
         if target_endpoint_ptr == 0 {
-            return (SyscallReturnStruct::new(SHARED_ENDPOINT_NOT_EXIST,pcid,cr3,pt_regs),None,None);
+            return (SyscallReturnStruct::new(SHARED_ENDPOINT_NOT_EXIST,pcid,cr3,pt_regs),None);
         }
         assert(self.proc_man.endpoint_perms@.dom().contains(target_endpoint_ptr));
 
         if self.proc_man.get_endpoint_rf_counter_by_endpoint_ptr(target_endpoint_ptr) == usize::MAX {
-            return (SyscallReturnStruct::new(SHARED_ENDPOINT_REF_COUNT_OVERFLOW,pcid,cr3,pt_regs),None,None);
+            return (SyscallReturnStruct::new(SHARED_ENDPOINT_REF_COUNT_OVERFLOW,pcid,cr3,pt_regs),None);
         }
 
         if self.page_alloc.free_pages.len() < 5 {
-            return (SyscallReturnStruct::new(SYSTEM_OUT_OF_MEM,pcid,cr3,pt_regs),None,None);
+            return (SyscallReturnStruct::new(SYSTEM_OUT_OF_MEM,pcid,cr3,pt_regs),None);
         }
 
         if self.proc_man.scheduler.len() == MAX_NUM_THREADS{
-            return (SyscallReturnStruct::new(SCHEDULER_NO_SPACE,pcid,cr3,pt_regs),None,None);
+            return (SyscallReturnStruct::new(SCHEDULER_NO_SPACE,pcid,cr3,pt_regs),None);
         }
 
         if self.proc_man.proc_ptrs.len() == MAX_NUM_PROCS {
-            return (SyscallReturnStruct::new(PROCESS_LIST_NO_SPEC,pcid,cr3,pt_regs),None,None);
+            return (SyscallReturnStruct::new(PROCESS_LIST_NO_SPEC,pcid,cr3,pt_regs),None);
         }
         if self.mmu_man.free_pcids.len() == 0 {
-            return (SyscallReturnStruct::new(NO_FREE_PCID,pcid,cr3,pt_regs),None,None);
+            return (SyscallReturnStruct::new(NO_FREE_PCID,pcid,cr3,pt_regs),None);
         }
         if self.mmu_man.free_ioids.len() == 0 {
-            return (SyscallReturnStruct::new(NO_FREE_IOID,pcid,cr3,pt_regs),None,None);
+            return (SyscallReturnStruct::new(NO_FREE_IOID,pcid,cr3,pt_regs),None);
         }
 
         let (page_ptr1, page_perm1) = self.page_alloc.alloc_pagetable_mem(); 
         let new_pcid = self.mmu_man.new_pagetable(page_ptr1,page_perm1,Some(self.kernel_pml4_entry));
         let (page_ptr2, page_perm2) = self.page_alloc.alloc_pagetable_mem(); 
         let new_ioid = self.mmu_man.new_iommutable(page_ptr2,page_perm2);
+        let iommutable_cr3 = self.mmu_man.get_cr3_by_ioid(new_ioid);
         let (page_ptr3, page_perm3) = self.page_alloc.alloc_kernel_mem(); 
         let (page_ptr4, page_perm4) = self.page_alloc.alloc_kernel_mem(); 
         let new_proc = self.proc_man.new_proc(page_ptr3, page_perm3, new_pcid, Some(new_ioid));
@@ -86,7 +88,7 @@ impl Kernel {
 
         assert(self.wf());
 
-        return (SyscallReturnStruct::new(SUCCESS,pcid,cr3,pt_regs),Some(new_proc),Some(new_thread));
+        return (SyscallReturnStruct::new(SUCCESS,pcid,cr3,pt_regs),Some(iommutable_cr3));
 
     }
 }
