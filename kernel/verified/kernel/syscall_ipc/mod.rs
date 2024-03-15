10eb1e1ff20df37666197ea4824779d1afb7957b
FeizaiYiHao <xiangdc@bu.edu>
2024-03-15 10:43:00 -0600

    ipc send message spec

--- kernel/verified/kernel/syscall_ipc/mod.rs
+++ kernel/verified/kernel/syscall_ipc/mod.rs
@@ -3,6 +3,7 @@ pub mod syscall_reply;
 pub mod syscall_reply_and_receive;
 pub mod syscall_send;
 pub mod syscall_send_empty;
+pub mod syscall_send_message;
 pub mod syscall_send_pages;
 
 pub use syscall_receive::*;
@@ -10,4 +11,5 @@ pub use syscall_reply::*;
 pub use syscall_reply_and_receive::*;
 pub use syscall_send::*;
 pub use syscall_send_empty::*;
+pub use syscall_send_message::*;
 pub use syscall_send_pages::*;
