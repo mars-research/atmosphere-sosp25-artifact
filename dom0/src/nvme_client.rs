432123edc93a115ef9d6a4e82f085309992f2bd2
Vikram Narayanan <vikram186@gmail.com>
2024-04-02 15:09:47 -0600

    nvme_client: add todos

--- dom0/src/nvme_client.rs
+++ dom0/src/nvme_client.rs
@@ -6,6 +6,7 @@ use libtime::sys_ns_loopsleep;
 use nvme_driver::device::NvmeDevice;
 use nvme_driver::nvme_test::{run_blocktest_blkreq, run_blocktest_raw_with_delay};
 use pcid::utils::PciBarAddr;
+use ring_buffer::{RingBuffer, SIZE_OF_BUFFER, SIZE_OF_QUEUE};
 
 pub fn test_nvme_driver() {
     let mut nvme_dev =
@@ -23,4 +24,42 @@ pub fn test_nvme_driver() {
     run_blocktest_blkreq(&mut nvme_dev);
 
     //test_nvme_ring_buffer();
+}
+
+#[repr(align(4096))]
+#[repr(C)]
+pub struct GenericRingBuffer<I, J> {
+    pub request_queue: RingBuffer<I, SIZE_OF_QUEUE>,
+    pub reply_queue: RingBuffer<J, SIZE_OF_QUEUE>,
+    pub free_stack: [usize; SIZE_OF_QUEUE],
+    pub data_buffer: [[usize; SIZE_OF_BUFFER]; SIZE_OF_QUEUE],
+    pub len: usize,
+}
+
+/// | MSG_TYPE | PAYLOAD |
+/// MSG_TYPE == STOP
+///     quit polling
+
+fn test_nvme_ring_buffer() {
+    loop {
+
+        // TODO:
+        // 1) Fill the queue with new BlockReq's
+        //      - Populate BlockReqs
+        // 2) Push the BlockReqs into the RingBuffer
+        // 3) Poll for BlockResps from the RingBuffer
+        // 4) Reclaim BlockReq from BlockResp
+        //      - Modify BlockReq's Op (read/write) and the block num
+    }
+}
+
+fn nvme_driver_backend() {
+    loop {
+
+        // TODO:
+        // 1) Poll for BlockReqs from the RingBuffer
+        // 2) Submit the BlockReqs to submit_and_poll_breq()
+        // 3) Collect the BlockReqs and convert it into BlockResps
+        // 4) Push the BlockResps into the RingBuffer
+    }
 }
