7fb21a995c1e9b93e3865df8d908a5b0ac3db18b
FeizaiYiHao <xiangdc@bu.edu>
2024-04-14 14:06:30 -0500

    add more ixgbe tests

--- dom0/src/ixgbe_client.rs
+++ dom0/src/ixgbe_client.rs
@@ -777,3 +777,391 @@ pub fn test_ixgbe_with_ring_buffer_single_core_two_processes(){
 }
 
 
+#[no_mangle]
+pub fn test_ixgbe_with_ring_buffer_tx_single_core_two_processes_ap()-> ! {
+    log::info!("hello from test_ixgbe_with_ring_buffer_single_core_two_processes_ap");
+
+    let sender_mac = [
+        0x90, 0xe2, 0xba, 0xb3, 0xbd, 0x99, // Dst mac
+    ];
+    let our_mac = [
+        // 90:E2:BA:B5:15:75
+        0x90, 0xe2, 0xba, 0xb5, 0x15, 0x75, // Src mac
+    ];
+
+    unsafe{
+        let size = core::mem::size_of::<IxgbeRingBuffer>();
+        let error_code = asys::sys_receive_pages(0,DATA_BUFFER_ADDR as usize, size / 4096 + 1);
+        if error_code != 0 {
+            log::info!("sys_receive_pages failed {:?}", error_code);
+        }else{
+            log::info!("data_buffer received by dom1");
+        }   
+
+        let buff_ref:&mut IxgbeRingBuffer = &mut*(DATA_BUFFER_ADDR as *mut IxgbeRingBuffer);
+
+        for i in 0..32 {
+            let (addr,paddr) = buff_ref.try_pop_allocator().unwrap();
+            // log::info!("receiving package at {:x}", addr);
+            unsafe {
+                ptr::copy(
+                    our_mac.as_ptr(),
+                    (addr + 6) as *mut u8, 
+                    our_mac.len(),
+                );
+                ptr::copy(
+                    sender_mac.as_ptr(),
+                    addr as *mut u8,
+                    sender_mac.len(),
+                );
+            }
+            buff_ref.reply_queue.try_push(&IxgbePayLoad{addr:addr, paddr:paddr, len:64});
+        }
+
+        let iter = 100000000;
+        let mut count = 0;
+        let print_iter = iter/20;
+        let mut print_count = 0;
+
+        while count <= iter {
+
+            if print_count >= print_iter{
+                log::info!("progress {}%", count as f64 / iter as f64 * 100.0);
+                print_count = 0;
+            }
+            for i in 0 .. 32 {
+                let pkt_op = buff_ref.reply_completion_queue.try_pop();
+                if pkt_op.is_none(){
+                    break;
+                }else{
+                    // log::info!("received package at {:x}", pkt_op.unwrap().addr);
+    
+                    count = count + 1;
+                    print_count = print_count + 1;
+        
+                    buff_ref.reply_queue.try_push(&pkt_op.unwrap());
+                    // log::info!("sending package at {:x}", pkt_op.unwrap().addr);
+                }
+            }
+
+            // log::info!("receiving from dom1");
+            let error_code = asys::sys_receive_empty(0);
+            // log::info!("pong");
+            if error_code != 0 {
+                log::info!("sys_send_empty failed {:?}", error_code);
+                break;
+            }
+        }
+
+        count = 0;
+
+        let start = _rdtsc();
+
+        while count <= iter {
+
+            if print_count >= print_iter{
+                log::info!("progress {}%", count as f64 / iter as f64 * 100.0);
+                print_count = 0;
+            }
+            for i in 0 .. 1 {
+                let pkt_op = buff_ref.reply_completion_queue.try_pop();
+                if pkt_op.is_none(){
+                    break;
+                }else{
+                    // log::info!("received package at {:x}", pkt_op.unwrap().addr);
+    
+                    count = count + 1;
+                    print_count = print_count + 1;
+        
+                    buff_ref.reply_queue.try_push(&pkt_op.unwrap());
+                    // log::info!("sending package at {:x}", pkt_op.unwrap().addr);
+                }
+            }
+
+            // log::info!("receiving from dom1");
+            let error_code = asys::sys_receive_empty(0);
+            // log::info!("pong");
+            if error_code != 0 {
+                log::info!("sys_send_empty failed {:?}", error_code);
+                break;
+            }
+        }
+
+        let end = _rdtsc();
+
+        log::info!("ixgbe cycles per tx {:?} throughput {:}",(end - start) as usize /iter, iter as u64 /((end - start)/2_600_000_000_u64));
+    }
+    loop{
+
+    }
+}
+
+pub fn test_ixgbe_with_ring_buffer_tx_single_core_two_processes(){
+unsafe {
+    let error_code = asys::sys_new_endpoint(0);
+        if error_code != 0 {
+            log::info!("sys_new_endpoint failed {:?}", error_code);
+            return;
+        }
+    let mut range = 0;
+    loop{
+        let (pa,perm) = asys::sys_mresolve(0x8000000000usize + range * 4096);
+        // log::info!("va:{:x?}, pa:{:x?}, perm:{:?}", 0x8000000000usize + range * 4096, pa, perm);
+        if perm == 34{
+            break;
+        }
+        range = range + 1;
+    }
+    log::info!("find {:?} pages until {:x?}", range, 0x8000000000usize + range * 4096);
+
+    let size = core::mem::size_of::<IxgbeRingBuffer>();
+    let error_code = asys::sys_mmap(DATA_BUFFER_ADDR as usize, 0x0000_0000_0000_0002u64 as usize, size / 4096 + 1);
+    if error_code != 0 {
+        log::info!("sys_mmap for data_buffer failed {:?}", error_code);
+        return;
+    }
+    
+    let buff_ref:&mut IxgbeRingBuffer = &mut*(DATA_BUFFER_ADDR as *mut IxgbeRingBuffer);
+    buff_ref.init();
+    log::info!("data_buffer init done");
+
+    let new_stack = 0x8000000000usize + range * 4096;
+    log::info!("allocating dom1 stack from {:x?}", new_stack);
+    let stack_size = 16 * 1024 * 1024;
+    let error_code = asys::sys_mmap(new_stack, 0x0000_0000_0000_0002u64 as usize, stack_size / 4096);
+    if error_code != 0 {
+        log::info!("sys_mmap for dom1 stack failed {:?}", error_code);
+        return;
+    }
+
+    let rsp: usize = new_stack + stack_size/2;
+    log::info!("request_queue address: {:x?}", rsp);  
+    let error_code = asys::sys_new_proc_with_iommu_pass_mem(0,test_ixgbe_with_ring_buffer_tx_single_core_two_processes_ap as *const () as usize, rsp, 0x8000000000usize, range + stack_size / 4096);
+        if error_code != 0 {
+            log::info!("sys_new_proc_with_iommu_pass_mem failed {:?}", error_code);
+            return;
+        }
+
+    let mut ixgbe_dev =
+        unsafe { IxgbeDevice::new(PciBarAddr::new(USERSPACE_BASE + 0xFE000000, 0x4000)) };
+
+    log::info!("Initializing Ixgbe driver...");
+
+    ixgbe_dev.init();
+
+
+    let error_code = asys::sys_send_pages(0,DATA_BUFFER_ADDR as usize, size / 4096 + 1);
+    if error_code == 5  {
+    }else{
+        if error_code == 0{
+            log::info!("data_buffer sent to dom1");
+        }else{
+            log::info!("sys_send_pages_no_wait failed {:?}", error_code);
+        }
+    }
+    
+    let buff_ref:&mut IxgbeRingBuffer = &mut*(DATA_BUFFER_ADDR as *mut IxgbeRingBuffer);
+    log::info!("ixgbe_backend started");
+    
+    loop{
+        ixgbe_dev.tx_submit_and_poll_with_ringbuffer_2(&mut buff_ref.reply_queue, &mut buff_ref.reply_completion_queue, false);
+
+        // log::info!("sending from dom0");
+        let error_code = asys::sys_send_empty(0);
+        // log::info!("sent from dom0");
+        if error_code != 0 {
+            log::info!("sys_send_empty failed {:?}", error_code);
+            return;
+        }
+
+    }
+}
+}
+
+#[no_mangle]
+pub fn test_ixgbe_with_ring_buffer_rx_single_core_two_processes_ap()-> ! {
+    log::info!("hello from test_ixgbe_with_ring_buffer_rx_single_core_two_processes_ap");
+
+    let sender_mac = [
+        0x90, 0xe2, 0xba, 0xb3, 0xbd, 0x99, // Dst mac
+    ];
+    let our_mac = [
+        // 90:E2:BA:B5:15:75
+        0x90, 0xe2, 0xba, 0xb5, 0x15, 0x75, // Src mac
+    ];
+
+    unsafe{
+        let size = core::mem::size_of::<IxgbeRingBuffer>();
+        let error_code = asys::sys_receive_pages(0,DATA_BUFFER_ADDR as usize, size / 4096 + 1);
+        if error_code != 0 {
+            log::info!("sys_receive_pages failed {:?}", error_code);
+        }else{
+            log::info!("data_buffer received by dom1");
+        }   
+
+        let buff_ref:&mut IxgbeRingBuffer = &mut*(DATA_BUFFER_ADDR as *mut IxgbeRingBuffer);
+
+        for i in 0..32 {
+            let (addr,paddr) = buff_ref.try_pop_allocator().unwrap();
+            // log::info!("receiving package at {:x}", addr);
+            buff_ref.request_queue.try_push(&IxgbePayLoad{addr:addr, paddr:paddr, len:4096});
+        }
+
+        let iter = 100000000;
+        let mut count = 0;
+        let print_iter = iter/20;
+        let mut print_count = 0;
+
+        while count <= iter {
+
+            if print_count >= print_iter{
+                log::info!("progress {}%", count as f64 / iter as f64 * 100.0);
+                print_count = 0;
+            }
+            for i in 0 .. 32 {
+                let pkt_op = buff_ref.request_completion_queue.try_pop();
+                if pkt_op.is_none(){
+                    break;
+                }else{
+                    // log::info!("received package at {:x}", pkt_op.unwrap().addr);
+    
+                    count = count + 1;
+                    print_count = print_count + 1;
+        
+                    buff_ref.request_queue.try_push(&pkt_op.unwrap());
+                    // log::info!("sending package at {:x}", pkt_op.unwrap().addr);
+                }
+            }
+
+            // log::info!("receiving from dom1");
+            let error_code = asys::sys_receive_empty(0);
+            // log::info!("pong");
+            if error_code != 0 {
+                log::info!("sys_send_empty failed {:?}", error_code);
+                break;
+            }
+        }
+
+        count = 0;
+
+        let start = _rdtsc();
+
+        while count <= iter {
+
+            if print_count >= print_iter{
+                log::info!("progress {}%", count as f64 / iter as f64 * 100.0);
+                print_count = 0;
+            }
+            for i in 0 .. 1 {
+                let pkt_op = buff_ref.request_completion_queue.try_pop();
+                if pkt_op.is_none(){
+                    break;
+                }else{
+                    // log::info!("received package at {:x}", pkt_op.unwrap().addr);
+    
+                    count = count + 1;
+                    print_count = print_count + 1;
+        
+                    buff_ref.request_queue.try_push(&pkt_op.unwrap());
+                    // log::info!("sending package at {:x}", pkt_op.unwrap().addr);
+                }
+            }
+
+            // log::info!("receiving from dom1");
+            let error_code = asys::sys_receive_empty(0);
+            // log::info!("pong");
+            if error_code != 0 {
+                log::info!("sys_send_empty failed {:?}", error_code);
+                break;
+            }
+        }
+
+        let end = _rdtsc();
+
+        log::info!("ixgbe cycles per rx {:?} throughput {:}",(end - start) as usize /iter, iter as u64 /((end - start)/2_600_000_000_u64));
+    }
+    loop{
+
+    }
+}
+
+pub fn test_ixgbe_with_ring_buffer_rx_single_core_two_processes(){
+unsafe {
+    let error_code = asys::sys_new_endpoint(0);
+        if error_code != 0 {
+            log::info!("sys_new_endpoint failed {:?}", error_code);
+            return;
+        }
+    let mut range = 0;
+    loop{
+        let (pa,perm) = asys::sys_mresolve(0x8000000000usize + range * 4096);
+        // log::info!("va:{:x?}, pa:{:x?}, perm:{:?}", 0x8000000000usize + range * 4096, pa, perm);
+        if perm == 34{
+            break;
+        }
+        range = range + 1;
+    }
+    log::info!("find {:?} pages until {:x?}", range, 0x8000000000usize + range * 4096);
+
+    let size = core::mem::size_of::<IxgbeRingBuffer>();
+    let error_code = asys::sys_mmap(DATA_BUFFER_ADDR as usize, 0x0000_0000_0000_0002u64 as usize, size / 4096 + 1);
+    if error_code != 0 {
+        log::info!("sys_mmap for data_buffer failed {:?}", error_code);
+        return;
+    }
+    
+    let buff_ref:&mut IxgbeRingBuffer = &mut*(DATA_BUFFER_ADDR as *mut IxgbeRingBuffer);
+    buff_ref.init();
+    log::info!("data_buffer init done");
+
+    let new_stack = 0x8000000000usize + range * 4096;
+    log::info!("allocating dom1 stack from {:x?}", new_stack);
+    let stack_size = 16 * 1024 * 1024;
+    let error_code = asys::sys_mmap(new_stack, 0x0000_0000_0000_0002u64 as usize, stack_size / 4096);
+    if error_code != 0 {
+        log::info!("sys_mmap for dom1 stack failed {:?}", error_code);
+        return;
+    }
+
+    let rsp: usize = new_stack + stack_size/2;
+    log::info!("request_queue address: {:x?}", rsp);  
+    let error_code = asys::sys_new_proc_with_iommu_pass_mem(0,test_ixgbe_with_ring_buffer_rx_single_core_two_processes_ap as *const () as usize, rsp, 0x8000000000usize, range + stack_size / 4096);
+        if error_code != 0 {
+            log::info!("sys_new_proc_with_iommu_pass_mem failed {:?}", error_code);
+            return;
+        }
+
+    let mut ixgbe_dev =
+        unsafe { IxgbeDevice::new(PciBarAddr::new(USERSPACE_BASE + 0xFE000000, 0x4000)) };
+
+    log::info!("Initializing Ixgbe driver...");
+
+    ixgbe_dev.init();
+
+
+    let error_code = asys::sys_send_pages(0,DATA_BUFFER_ADDR as usize, size / 4096 + 1);
+    if error_code == 5  {
+    }else{
+        if error_code == 0{
+            log::info!("data_buffer sent to dom1");
+        }else{
+            log::info!("sys_send_pages_no_wait failed {:?}", error_code);
+        }
+    }
+    
+    let buff_ref:&mut IxgbeRingBuffer = &mut*(DATA_BUFFER_ADDR as *mut IxgbeRingBuffer);
+    log::info!("ixgbe_backend started");
+    
+    loop{
+        ixgbe_dev.rx_submit_and_poll_with_ringbuffer_2(&mut buff_ref.request_queue, &mut buff_ref.request_completion_queue, false);
+
+        // log::info!("sending from dom0");
+        let error_code = asys::sys_send_empty(0);
+        // log::info!("sent from dom0");
+        if error_code != 0 {
+            log::info!("sys_send_empty failed {:?}", error_code);
+            return;
+        }
+    }
+}
+}
