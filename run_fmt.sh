71a8c91bc4d1d941a702e13f2eab055006da0f3f
FeizaiYiHao <xiangdc@bu.edu>
2024-11-30 12:41:36 -0700

    add script for running verusfmt

---
+++ run_fmt.sh
@@ -0,0 +1,13 @@
+# !/bin/bash
+
+verusfmt ./kernel/verified/util/*
+verusfmt ./kernel/verified/kernel/*
+verusfmt ./kernel/verified/allocator/*
+verusfmt ./kernel/verified/slinkedlist/*
+verusfmt ./kernel/verified/process_manager/*
+verusfmt ./kernel/verified/memory_manager/*
+verusfmt ./kernel/verified/pagetable/*
+verusfmt ./kernel/verified/lemma/*
+verusfmt ./kernel/verified/trap.rs
+verusfmt ./kernel/verified/va_range.rs
+verusfmt ./kernel/verified/define.rs
